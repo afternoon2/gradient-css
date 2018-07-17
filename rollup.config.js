65ceca5fa1e2121cf7a334ed71cc8a011fcddc7b
Jakub Antolak <poprostuantolak@gmail.com>
2018-07-17 15:41:29 +0200

    add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,51 @@
+import babel from 'rollup-plugin-babel'
+import cleanup from 'rollup-plugin-cleanup'
+import commonjs from 'rollup-plugin-commonjs'
+import eslint from 'rollup-plugin-eslint'
+import resolve from 'rollup-plugin-node-resolve'
+import eslintConf from './.eslintrc.json'
+import { terser } from 'rollup-plugin-terser'
+
+const babelConf = {
+    exclude: ['/node_modules/'],
+    plugins: ['external-helpers']
+}
+
+export default {
+    input: 'index.js',
+    external: ['chroma-js', 'gradient-base'],
+    globals: {
+        'chroma-js': 'chroma',
+        'gradient-base': 'Base'
+    },
+    output: [
+        {
+            extend: true,
+            format: 'umd',
+            file: './dist/gradient-css.umd.js',
+            name: 'Css',
+            sourcemap: true
+        },
+        {
+            extend: true,
+            format: 'es',
+            file: './dist/gradient-css.mjs',
+            name: 'Css',
+            sourcemap: true
+        }
+    ],
+    watch: {
+        include: 'src/**'
+    },
+    plugins: [
+        resolve(),
+        eslint(eslintConf),
+        babel(babelConf),
+        cleanup({
+            comments: 'none',
+            extensions: '.js'
+        }),
+        commonjs(),
+        terser()
+    ]
+}
