58c793ee50854e51b8ca210082e6409e4ceb2d9e
Jakub Antolak <poprostuantolak@gmail.com>
2018-07-29 11:02:19 +0200

    tests + css module

---
+++ index.js
@@ -0,0 +1,140 @@
+import Base from 'gradient-base'
+
+/**
+ * @typedef {object} CssOptions - CssOverlay component's configuration object
+ * @property {string} type - 'linear' or 'radial'
+ * @property {number} [angle] - linear gradient's angle value
+ * @property {number} [left] - radial gradient's position on the x axis
+ * @property {number} [top] - radial gradient's position on the y axis
+ * @property {string} [shape] - radial gradient's shape: 'ellipse' or 'circle' (mandatory for radial gradient)
+ * @property {string} [extent] - radial gradient's extent keyword
+ */
+
+/**
+ * @class CssOverlay
+ * @extends Overlay
+ * @classdesc
+ * @param {number[][]} colors
+ * @param {CssOptions} options
+ * Css overlay class that returns single gradient strings
+ */
+export default class Css {
+    constructor(colors, options) {
+        /**
+         * @property {Base} _base
+         * @private
+         */
+        this._base = new Base(colors, options.base)
+        
+        /**
+         * @property {number[][]} _rawColors
+         * @private
+         */
+        this._rawColors = this._base.get()
+
+        /**
+         * @property {Colors} colors
+         */
+        this.colors = this._base.get()
+
+        /**
+         * @property {CssOptions} options
+         */
+        this.options = options
+    }
+
+    /**
+     * @returns {string}
+     * @description
+     * Returns single css gradient string
+     */
+    get() {
+        return `${this.options.css.type}-gradient(${this._angle}${this._shape}${this._extent}${this._stringifyColors()})`
+    }
+
+    /**
+     * @returns {string}
+     * @private
+     */
+    get _angle() {
+        return this.options.css.angle && this.options.css.type === 'linear' ?
+            this.options.css.angle + 'deg, ' :
+            ''
+    }
+
+    /**
+     * @returns {string}
+     * @private
+     */
+    get _shape() {
+        if (this.options.css.type === 'radial') {
+            if (
+                this.options.css.shape &&
+                this.options.css.shape !== 'ellipse' &&
+                (!this.options.css.top && !this.options.css.left)
+            ) {
+                return this.options.css.shape + ', '
+            }
+            if (
+                this.options.css.shape &&
+                this.options.css.shape !== 'ellipse' &&
+                (this.options.css.top && this.options.css.left)
+            ) {
+                return `${this.options.css.shape} at ${this.options.css.left}% ${this.options.css.top}%, `
+            }
+            if (
+                this.options.css.shape &&
+                this.options.css.shape === 'ellipse' &&
+                (this.options.css.top && this.options.css.left) &&
+                this.options.css.extent === 'none'
+            ) {
+                return `${this.options.css.shape} at ${this.options.css.left}% ${this.options.css.top}%, `
+            }
+            if (
+                this.options.css.shape &&
+                this.options.css.shape === 'ellipse' &&
+                (this.options.css.top && this.options.css.left) &&
+                this.options.css.extent !== 'none'
+            ) {
+                return ''
+            }
+        }
+        return ''
+    }
+
+    /**
+     * @returns {string}
+     * @private
+     */
+    get _extent() {
+        const afterExtent = this.options.css.top && this.options.css.left ?
+            ` at ${this.options.css.left}% ${this.options.css.top}%, ` :
+            ', '
+        if (
+            this.options.css.shape === 'ellipse' &&
+            this.options.css.extent &&
+            this.options.css.extent !== 'none'
+        ) {
+            return this.options.css.extent + afterExtent
+        } else if (
+            this.options.css.shape === 'ellipse' &&
+            !this.options.css.extent &&
+            this.options.css.top &&
+            this.options.css.left
+        ) {
+            return afterExtent
+        }
+        return ''
+    }
+
+    /**
+     * @returns {string[]}
+     * @description
+     * Maps rgba arrays to rgba css strings
+     * @private
+     */
+    _stringifyColors() {
+        return this.colors.map(color => `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${color[3]})`)
+    }
+    
+}
---
+++ index.test.js
@@ -0,0 +1,151 @@
+import Css from './index'
+
+test(
+    'If gradient-css generates correct css linear gradient',
+    () => {
+        const css = new Css([
+            [10, 220, 33, 0.1],
+            [254, 200, 10, 1]
+        ], {
+            base: {
+                interpolation: 'linear',
+                samples: 10,
+                mode: 'hsl',
+                lightessCorrection: true
+            },
+            css: {
+                type: 'linear'
+            }
+        })
+        const regexp = /linear-gradient\(((rgba\(([0-9]+\,\s?)+([0-9]?\.?[0-9]+\)\,?\s?))+)\)/
+        const gradient = css.get()
+        expect(regexp.test(gradient)).toBe(true)
+    }
+)
+
+test(
+    'If css overlay returns correct linear gradient with angle',
+    () => {
+        const css = new Css([
+            [10, 220, 33, 0.1],
+            [254, 200, 10, 1]
+        ], {
+            base: {
+                interpolation: 'linear',
+                samples: 10,
+                mode: 'hsl',
+                lightessCorrection: true
+            },
+            css: {
+                type: 'linear',
+                angle: 90
+            }
+        })
+        const regexp = /linear-gradient\(([0-9]+deg\,\s?)(rgba\(([0-9]+\,\s?)+([0-9]?\.?[0-9]+\)\,?\s?))+\)/
+        const gradient = css.get()
+        expect(regexp.test(gradient)).toBe(true)
+    }
+)
+
+test(
+    'If gradient-css generates correct css radial gradient with the shape of a circle',
+    () => {
+        const css = new Css([
+            [10, 220, 33, 0.1],
+            [254, 200, 10, 1]
+        ], {
+            base: {
+                interpolation: 'linear',
+                samples: 10,
+                mode: 'hsl',
+                lightessCorrection: true
+            },
+            css: {
+                type: 'radial',
+                shape: 'circle'
+            }
+        })
+        const regexp = /radial-gradient\((circle\,\s?)(rgba\(([0-9]+\,\s?)+([0-9]?\.?[0-9]+\)\,?\s?))+\)/
+        const gradient = css.get()
+        expect(regexp.test(gradient)).toBe(true)
+    }
+)
+
+test(
+    'If gradient-css generates correct css radial gradient with the shape of a circle and the position',
+    () => {
+        const css = new Css([
+            [10, 220, 33, 0.1],
+            [254, 200, 10, 1]
+        ], {
+            base: {
+                interpolation: 'linear',
+                samples: 10,
+                mode: 'hsl',
+                lightessCorrection: true
+            },
+            css: {
+                type: 'radial',
+                shape: 'circle',
+                top: 30,
+                left: 20
+            }
+        })
+        const regexp = /radial-gradient\((circle\s?)(at\s([0-9]+%)\s[0-9]+%\,\s)(rgba\(([0-9]+\,\s?)+([0-9]?\.?[0-9]+\)\,?\s?))+\)/
+        const gradient = css.get()
+        expect(regexp.test(gradient)).toBe(true)
+    }
+)
+
+test(
+    'If gradient-css generates correct css radial gradient with the shape of a ellipse, the position and the extent keyword',
+    () => {
+        const css = new Css([
+            [10, 220, 33, 0.1],
+            [254, 200, 10, 1]
+        ], {
+            base: {
+                interpolation: 'linear',
+                samples: 10,
+                mode: 'hsl',
+                lightessCorrection: true
+            },
+            css: {
+                type: 'radial',
+                top: 30,
+                left: 20,
+                shape: 'ellipse',
+                extent: 'farthest-corner'
+            }
+        })
+        const regexp = /radial-gradient\(((farthest|closest)-(side|corner)\,?\s?)(at(\s[0-9]+%)+\,?\s?)(rgba\(([0-9]+\,\s?)+([0-9]?\.?[0-9]+\)\,?\s?))+\)/
+        const gradient = css.get()
+        expect(regexp.test(gradient)).toBe(true)
+    }
+)
+
+test(
+    'If gradient-css generates correct css radial gradient with the shape of a ellipse, the position and the extent keyword',
+    () => {
+        const css = new Css([
+            [10, 220, 33, 0.1],
+            [254, 200, 10, 1]
+        ], {
+            base: {
+                interpolation: 'linear',
+                samples: 10,
+                mode: 'hsl',
+                lightessCorrection: true
+            },
+            css: {
+                type: 'radial',
+                top: 30,
+                left: 20,
+                shape: 'ellipse'
+            }
+        })
+        const regexp = /radial-gradient\((\s?at(\s[0-9]+%)+\,?\s?)(rgba\(([0-9]+\,\s?)+([0-9]?\.?[0-9]+\)\,?\s?))+\)/
+        const gradient = css.get()
+        expect(regexp.test(gradient)).toBe(true)
+    }
+)
